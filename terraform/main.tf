cee0d50e6cc6eb46b39a1ccb36bc9d5bd7f43d3d
Paul Adegoke <farmerpisco@gmail.com>
2026-01-25 23:05:29 +0100

    Improve terraform file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,13 @@
 terraform {
   required_version = ">= 1.0"
 
-  backend "s3" {
-    bucket         = "starttech-state-bucket"
-    key            = "starttech/terraform.tfstate"
-    region         = "eu-west-2"
-    encrypt        = true
-    dynamodb_table = "starttech-terraform-locks"
-  }
+  # backend "s3" {
+  #   bucket         = "starttech-state-bucket"
+  #   key            = "starttech/terraform.tfstate"
+  #   region         = "eu-west-2"
+  #   encrypt        = true
+  #   dynamodb_table = "starttech-terraform-locks"
+  # }
 
   required_providers {
     aws = {
