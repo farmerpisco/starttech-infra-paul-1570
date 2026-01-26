7b363aabd40011ffb91bd647e3919ccf45cc874b
Paul Adegoke <farmerpisco@gmail.com>
2026-01-26 11:53:26 +0100

    Create outputs for storage resources

--- terraform/modules/storage/outputs.tf
+++ terraform/modules/storage/outputs.tf
@@ -1,3 +1,13 @@
 output "redis_endpoint" {
   value = aws_elasticache_cluster.redis.cache_nodes[0].address
 }
+
+output "s3_bucket_name" {
+  description = "Name of S3 bucket created for frontend files"
+  value       = aws_s3_bucket.st_s3.bucket
+}
+
+output "cloudfront_dist_id" {
+  description = "Cloundfront distribution ID"
+  value       = aws_cloudfront_distribution.st_frontend_cdn.id
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,3 +6,13 @@ output "alb_dns_name" {
 output "redis_endpoint" {
   value = module.storage.redis_endpoint
 }
+
+output "s3_bucket_name" {
+  description = "Name of S3 bucket created for frontend files"
+  value       = module.storage.s3_bucket_name
+}
+
+output "cloudfront_dist_id" {
+  description = "Cloundfront distribution ID"
+  value       = module.storage.cloudfront_dist_id
+}
