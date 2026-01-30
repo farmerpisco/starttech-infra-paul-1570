1f0bb36c4c9a6b0c9fc233e8e58c93099b757b8a
Paul Adegoke <farmerpisco@gmail.com>
2026-01-30 20:05:19 +0100

    Fix bugs in terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,20 +34,20 @@ module "compute" {
   project_name              = var.project_name
   key_name                  = var.key_name
   instance_type             = var.instance_type
-  vpc_id                    = var.vpc_id
-  st_sg_id                  = var.st_sg_id
-  st_alb_sg_id              = var.st_alb_sg_id
-  iam_instance_profile_name = var.iam_instance_profile_name
-  private_subnets_ids       = var.private_subnets_ids
-  public_subnets_ids        = var.public_subnets_ids
+  vpc_id                    = module.networking.st_vpc_id
+  st_sg_id                  = module.networking.st_sg_id
+  st_alb_sg_id              = module.networking.st_alb_sg_id
+  iam_instance_profile_name = module.monitoring.st_ec2_profile_name
+  private_subnets_ids       = module.networking.st_private_subnets_ids
+  public_subnets_ids        = module.networking.st_public_subnets_ids
 }
 
 module "storage" {
   source = "./modules/storage"
 
   project_name       = var.project_name
-  redis_sg_id        = var.redis_sg_id
-  private_subnet_ids = var.private_subnets_ids
+  redis_sg_id        = module.networking.redis_sg_id
+  private_subnet_ids = module.networking.st_private_subnets_ids
 }
 
 module "monitoring" {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -21,45 +21,8 @@ variable "key_name" {
   type        = string
   default     = "AltSchoolDemo"
 }
-
-variable "vpc_id" {
-  description = "VPC ID for the compute module"
+variable "instance_type" {
+  description = "EC2 instance type"
   type        = string
-  default     = module.networking.st_vpc_id
-}
-
-variable "st_sg_id" {
-  description = "Security group ID for the compute module"
-  type        = string
-  default     = module.networking.st_sg_id
-}
-
-variable "st_alb_sg_id" {
-  description = "Security group ID for the ALB in the compute module"
-  type        = string
-  default     = module.networking.st_alb_sg_id
-}
-
-variable "iam_instance_profile_name" {
-  description = "IAM instance profile name for the compute module"
-  type        = string
-  default     = module.monitoring.st_ec2_profile_name
-}
-
-variable "private_subnets_ids" {
-  description = "Private subnets IDs for the compute module"
-  type        = list(string)
-  default     = module.networking.st_private_subnets_ids
-}
-
-variable "public_subnets_ids" {
-  description = "Public subnets IDs for the compute module"
-  type        = list(string)
-  default     = module.networking.st_public_subnets_ids
-}
-
-variable "redis_sg_id" {
-  description = "Security group ID for Redis in the storage module"
-  type        = string
-  default     = module.networking.redis_sg_id
+  default     = "t3.micro"
 }
