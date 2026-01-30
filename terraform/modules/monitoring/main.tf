a36f5ccd216c4c90004a5ad576c6ddecd1be95c4
Paul Adegoke <farmerpisco@gmail.com>
2026-01-31 00:04:35 +0100

    Improve project

--- terraform/modules/monitoring/main.tf
+++ terraform/modules/monitoring/main.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudwatch_log_group" "st_cw_lg" {
   name              = "/${var.project_name}/backend"
-  retention_in_days = 14
+  retention_in_days = 7
 
   tags = {
     Environment = "dev"
